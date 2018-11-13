ec31fe01b7cbadbc7ddf663ba2316cae15661c9e
Sean Saoirse <seansaoirse@icloud.com>
2018-11-13 19:55:08 +0700

    Initial Commit

---
+++ NewTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewTalk
+//
+//  Created by Sean Saoirse on 13/11/18.
+//  Copyright © 2018 Sean Saoirse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
